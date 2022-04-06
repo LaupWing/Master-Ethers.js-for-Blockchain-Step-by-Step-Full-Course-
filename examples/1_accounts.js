95f3986d1817d26bfa32d14ea8f713c3b5d6afd7
LaupWing <loc_strict@hotmail.com>
2022-04-06 14:41:01 +0200

    update

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -2,6 +2,12 @@ const {ethers} = require('ethers')
 require('dotenv').config()
 
 console.log(process.env)
-const provider = new ethers.providers.JsonRpcProvider(`https://${process.env.API_KEY}@eth2-beacon-mainnet.infura.io`)
+const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${process.env.API_KEY}`)
 
-console.log(provider)
+const main = async ()=>{
+   const balance = await provider.getBalance('0x00b0718d8147D27222959e9fE01296b9761C9460')
+   console.log(balance)
+}
+
+
+main()
