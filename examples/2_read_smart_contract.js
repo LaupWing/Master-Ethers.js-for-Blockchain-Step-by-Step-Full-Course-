24170ba92d18e3276ef04635b8c7da3c667404b0
LaupWing <loc_strict@hotmail.com>
2022-04-06 15:01:04 +0200

    2

--- examples/2_read_smart_contract.js
+++ examples/2_read_smart_contract.js
@@ -1,9 +1,31 @@
-const {ethers} = require('ethers')
+const { ethers } = require('ethers')
 require('dotenv').config()
 const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${process.env.API_KEY}`)
 
+const ERC20_ABI = [
+   'function name() view returns (string)',
+   'function symbol() view returns (string)',
+   'function totalSupply() view returns (uint256)',
+   'function balanceOf(address) view returns (uint)'
+]
 
-const main = async ()=>{
+const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
+const contract = new ethers.Contract(address, ERC20_ABI, provider)
+
+
+const main = async () => {
+   const name = await contract.name()
+   const symbol = await contract.symbol()
+   const totalSupply = await contract.totalSupply()
+
+   console.log(symbol)
+   console.log(name)
+   console.log(ethers.utils.formatEther(totalSupply))
+
+   const balance = await contract.balanceOf('0x6c6Bc977E13Df9b0de53b251522280BB72383700')
+
+   console.log(`Balance Returned: ${balance}`)
+   console.log(`Balance Formatted: ${ethers.utils.formatEther(balance)}\n`)
 }
 
 
