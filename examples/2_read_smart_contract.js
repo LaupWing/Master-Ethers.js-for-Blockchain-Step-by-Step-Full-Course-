325a5c60b3d194a16bc6d5993321b0f60e81b90e
LaupWing <loc_strict@hotmail.com>
2022-04-06 14:48:23 +0200

    clean start

---
+++ examples/2_read_smart_contract.js
@@ -0,0 +1,10 @@
+const {ethers} = require('ethers')
+require('dotenv').config()
+const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${process.env.API_KEY}`)
+
+
+const main = async ()=>{
+}
+
+
+main()
