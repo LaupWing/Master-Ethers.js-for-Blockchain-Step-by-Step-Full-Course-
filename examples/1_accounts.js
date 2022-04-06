b37ba08574a5277cc91fc54f954df12068aeb9e2
LaupWing <loc_strict@hotmail.com>
2022-04-06 14:37:11 +0200

    dot env

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -1,3 +1,7 @@
 const {ethers} = require('ethers')
+require('dotenv').config()
 
+console.log(process.env)
 const provider = new ethers.providers.JsonRpcProvider(`https://${process.env.API_KEY}@eth2-beacon-mainnet.infura.io`)
+
+console.log(provider)
